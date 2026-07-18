1ada61488d6c2de64168e2f9fdcb75362dc3933d
avigunjal07 <avishkargunjal07@gmail.com>
2026-07-18 19:53:16 +0530

    Simple fibonacci problem

---
+++ Fibonnaci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Fibonnaci{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=0;
+        int b=1;
+        int c=0;
+
+        System.out.print("Enter a number : ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+            
+        }
+    }
+}
